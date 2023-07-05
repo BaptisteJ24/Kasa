344a2152dbff03b761dbc4f405a341e4c930d651
BaptisteJ24 <baptistejan@outlook.fr>
2023-07-05 10:09:34 +0200

    fix logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import { NavLink, Link } from 'react-router-dom';
-import logo from './../../assets/Logo.svg';
+import logo from './../../assets/logo.svg';
 // import '../../styles/Header.scss';
 
 const Header = () => {
